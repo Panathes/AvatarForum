12615179bbd2415a344133e4667b3c393ddf47e8
Guillaume <guillaume-cornet07@hotmail.fr>
2021-05-25 16:42:38 +0200

    create pdo connection db

--- application/api/src/Infrastructure/Db.php
+++ application/api/src/Infrastructure/Db.php
@@ -2,10 +2,44 @@
 
 namespace App\Infrastructure;
 
+use PDO;
+
 class Db
 {
-    public function __construct()
-    {
+    protected $host = '127.0.0.1';
+    protected $dbname = 'avatar';
+    protected $user = 'user';
+    protected $password = 'admin';
 
+    private static $connection;
+
+    /**
+     * Db constructor.
+     * @param string $host
+     * @param string $dbname
+     * @param string $user
+     * @param string $password
+     */
+    public function __construct($host, $dbname, $user, $password)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->dbname = $dbname;
+        $this->password = $password;
+    }
+
+    /**
+     *
+     */
+    public function dbConnect()
+    {
+        try {
+            self::$connection = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.'', $this->user, $this->password);
+        } catch (PDOException $exception) {
+            die($exception->getMessage());
+        }
+        self::$connection->exec('SET NAMES UTF8');
+
+        return self::$connection;
     }
 }
